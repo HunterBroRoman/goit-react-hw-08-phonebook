e9c9567526121715c9772e8a35ef2c1163c9c084
Roman <sayga76239@gmail.com>
2023-03-24 11:37:32 +0100

    utility

---
+++ src/utils/Container.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  min-height: calc(100vh - 160px);
+  display: flex;
+  padding: 20px 30px;
+  background-color: #f3f3f3;
+  color: #333;
+  &:link {
+    text-decoration: none;
+  }
+`;
